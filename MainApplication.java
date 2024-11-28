b2e084cf8e904fb608c573bf724f722ea1ecf845
Pornphipat <pornphipat.pho@student.mahidol.edu>
2024-11-28 20:26:06 +0700

    Yed Mae Gu Pause Game Dai Leaw

--- MainApplication.java
+++ MainApplication.java
@@ -6,7 +6,7 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 import java.awt.event.ActionEvent; // ADD
 import java.awt.event.ActionListener; // ADD
-
+ 
 public class MainApplication extends JFrame implements KeyListener {
     private JPanel              contentpane;
     private JLabel              drawpane, timerLabel;
@@ -24,6 +24,9 @@ public class MainApplication extends JFrame implements KeyListener {
     private int framewidth  = Constants.FRAME_WIDTH;
     private int frameheight = Constants.FRAME_HEIGHT;
     private int timeRemaining = 60;
+    private boolean isPaused = false;
+    private JFrame pauseFrame; // ADD
+
 
 
     ///////////////////////////////////////////////
@@ -52,8 +55,8 @@ public class MainApplication extends JFrame implements KeyListener {
     
     public void AddComponents()
     {
-            //    backgroundImg = new ImageIcon(Constants.FILE_BGGAME).resize(framewidth, frameheight);
-            //    drawpane.setIcon(backgroundImg);
+        //    backgroundImg = new ImageIcon(Constants.FILE_BGGAME).resize(framewidth, frameheight);
+        //    drawpane.setIcon(backgroundImg);
         drawpane = new JLabel();
         drawpane.setLayout(null);
         
@@ -76,7 +79,7 @@ public class MainApplication extends JFrame implements KeyListener {
 
         addKeyListener(this);
         startCountdownTimer(); // ADD
-        ToppingThread();
+        AddTopping();
     }
 
     private void startCountdownTimer() { // ADD
@@ -99,79 +102,121 @@ public class MainApplication extends JFrame implements KeyListener {
         System.exit(0);
     }
 
-
-    public void ToppingThread()
+    
+    public void PauseGame()
     {
-        Thread toppingSpawner = new Thread(){
-            public void run(){
-                while (true){
-                        ToppingLabel toppingLabel = new ToppingLabel(currentFrame);
-                        drawpane.add(toppingLabel);
-                        drawpane.repaint();
-
-                        //Start a new thread to animate each topping's fall
-                        Thread toppingFallingThread = new Thread(){
-                            public void run()
-                            {
-                                while (!toppingLabel.isGet() && toppingLabel.curY < frameheight) {
-                                //Move topping down
-                                toppingLabel.curY += 20;
-                                toppingLabel.setLocation(toppingLabel.curX, toppingLabel.curY);
-                                //Check for intersection
-                                if (bowlLabel.getBounds().intersects(toppingLabel.getBounds())) {
-                                    toppingLabel.setGet(true);
-                                    toppingLabel.playGetSound();
-                                    drawpane.remove(toppingLabel);
-                                    drawpane.repaint();
-                                    break;
-                            }
-                            try{
-                                Thread.sleep(toppingLabel.speed); //control falling speed
-                            } catch (InterruptedException e){
-                                e.printStackTrace();
-                            }
-                        }
-
-                        //cleanup when topping exits the screen
-                        if (!toppingLabel.isGet()) {
-                            drawpane.remove(toppingLabel);
-                            drawpane.repaint();
-                        }
-                    }
-                };
-                toppingFallingThread.start();
-
-                //Wait before spawning the next topping
-                    try {
-                        Thread.sleep(1500);
-                    } catch (InterruptedException e){
-                        e.printStackTrace();
-                    }
+        isPaused = !isPaused; // Toggle pause state
+        // System.out.println("Pause state: " + isPaused);
+        if (isPaused) {
+            themeSound.stop();
+            pauseFrame = new JFrame(); // Initialize pause frame
+            pauseFrame.setSize(300, 200);
+            pauseFrame.setLocationRelativeTo(null);
+            pauseFrame.setVisible(true);
+            JButton resumeButton = new JButton("Resume");
+            resumeButton.addActionListener(new ActionListener() {
+                @Override
+                public void actionPerformed(ActionEvent e) {
+                    PauseGame(); // Resume the game
                 }
-            }
-        };
-        toppingSpawner.start();
-    }
+            });
+            pauseFrame.add(resumeButton);
 
+        } else {
+            themeSound.playLoop();
+            if (pauseFrame != null) {
+                pauseFrame.dispose(); // Close pause frame
+            }
+        }
+    }
+    
     @Override
     public void keyPressed(KeyEvent e) {
-
+        
         switch (e.getKeyCode()){
             case KeyEvent.VK_RIGHT :
-                bowlLabel.moveRight();
-                break;
+            bowlLabel.moveRight();
+            break;
             case KeyEvent.VK_LEFT:
-                bowlLabel.moveLeft();
-                break;
-
+            bowlLabel.moveLeft();
+            break;
+            case KeyEvent.VK_ESCAPE:
+            PauseGame();
+            break;
+            
         }
     }
     @Override
     public void keyTyped(KeyEvent e) {}
     @Override
     public void keyReleased(KeyEvent e) {}
+    
 
+    public void AddTopping()
+    {
+        Thread toppingSpawner = new Thread(){
+            public void run(){
+                while (true){
+                    if(!isPaused){
+                        ToppingLabel toppingLabel = new ToppingLabel(currentFrame);
+                        drawpane.add(toppingLabel);
+                        drawpane.repaint();
+                        
+                        ToppingFall(toppingLabel);
+                        
+                        //Wait before spawning the next topping
+                        try {
+                            Thread.sleep(1500);
+                        } catch (InterruptedException e){
+                            e.printStackTrace();
+                        }
+                    }
+                    // System.out.println("Paused: " + isPaused);
+                }
+            }
+        };
+        toppingSpawner.start();
+    }
+    
+    //Start a new thread to animate each topping's falling
+    public void ToppingFall(ToppingLabel toppingLabel){
+        Thread toppingFallingThread = new Thread(){
+            public void run()
+            {
+                while (!toppingLabel.isGet() && toppingLabel.curY < frameheight) 
+                {
+                    if(!isPaused) {
+                        //Move topping down
+                        toppingLabel.curY += 20;
+                        toppingLabel.setLocation(toppingLabel.curX, toppingLabel.curY);
+                    } 
+                    else{
+                        toppingLabel.setLocation(toppingLabel.curX, toppingLabel.curY);
+                    }
+                    //Check for intersection
+                    if (bowlLabel.getBounds().intersects(toppingLabel.getBounds())) {
+                        toppingLabel.setGet(true);
+                        toppingLabel.playGetSound();
+                        drawpane.remove(toppingLabel);
+                        drawpane.repaint();
+                        break;
+                    }
+                    try{
+                        Thread.sleep(toppingLabel.speed); //control falling speed
+                    } catch (InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+    
+                //cleanup when topping exits the screen
+                if (!toppingLabel.isGet()) {
+                    drawpane.remove(toppingLabel);
+                    drawpane.repaint();
+                }
+            }
+        };
+        toppingFallingThread.start();
+    }
 }
-
 
 
